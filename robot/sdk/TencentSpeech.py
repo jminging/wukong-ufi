c07f3400e0bb9e039b8da89510aad73446c4b18c
jminging <jminging88@gmail.com>
2023-04-25 15:47:38 +0800

    升级ASR请求版本。

--- robot/sdk/TencentSpeech.py
+++ robot/sdk/TencentSpeech.py
@@ -3,6 +3,10 @@
 
 
 "Tencent ASR && TTS API"
+import hashlib
+
+from datetime import datetime
+
 __author__ = "Charles Li, Joseph Pan"
 
 import time
@@ -153,7 +157,6 @@ class tencentSpeech(object):
         config_dict = {
             "Action": "TextToVoice",
             "Version": "2018-05-22",
-            "ProjectId": 0,
             "Region": self.Region,
             "VoiceType": self.VoiceType,
             "Timestamp": int(time.time()),
@@ -191,33 +194,26 @@ class tencentSpeech(object):
         return self.oneSentenceRecognition()
 
     def oneSentenceRecognition(self):
-        # 生成body
-        def make_body(config_dict, sign_encode):
-            ##注意URL编码的时候分str编码，整段编码会丢data
-            body = ""
-            for a, b in config_dict:
-                body += urllib.parse.quote(a) + "=" + urllib.parse.quote(str(b)) + "&"
-            return body + "Signature=" + sign_encode
+        # # 生成body
+        # def make_body(config_dict, sign_encode):
+        #     ##注意URL编码的时候分str编码，整段编码会丢data
+        #     body = ""
+        #     for a, b in config_dict:
+        #         body += urllib.parse.quote(a) + "=" + urllib.parse.quote(str(b)) + "&"
+        #     return body + "Signature=" + sign_encode
 
-        HOST = "aai.tencentcloudapi.com"
+        HOST = "asr.tencentcloudapi.com"
         config_dict = {
-            "Action": "SentenceRecognition",
-            "Version": "2018-05-22",
-            "Region": self.Region,
             "ProjectId": 0,
             "SubServiceType": 2,
-            "EngSerViceType": "16k",
+            "EngSerViceType": "16k_zh_dialect",
             "VoiceFormat": self.VoiceFormat,
-            "UsrAudioKey": random.randint(0, 20),
-            "Timestamp": int(time.time()),
-            "Nonce": random.randint(100000, 200000),
-            "SecretId": self.SECRET_ID,
-            "SourceType": self.SourceType,
+            "SourceType": int(self.SourceType),
         }
         if self.SourceType == "0":
             config_dict["Url"] = urllib.parse.quote(str(self.url))
         else:
-            # 不能大于1M
+            # 音频频时长不能超过60s，音频文件大小不能超过3MB。
             file_path = self.URL
             file = open(file_path, "rb")
             content = file.read()
@@ -225,26 +221,84 @@ class tencentSpeech(object):
             config_dict["Data"] = base64.b64encode(content).decode()
             # config_dict['Data'] = content
             file.close()
-        # 按key排序
-        config_dict = sorted(config_dict.items())
-        signstr = self.formatSignString(config_dict)
-        sign_encode = urllib.parse.quote(self.encode_sign(signstr, self.SECRET_KEY))
-        body = make_body(config_dict, sign_encode)
+        body = config_dict
         # Get URL
-        req_url = "https://aai.tencentcloudapi.com"
+        req_url = "https://" + HOST
         header = {
             "Host": HOST,
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Charset": "UTF-8",
+            "Content-Type": "application/json; charset=utf-8",
+            "X-TC-Action": "SentenceRecognition",
+            "X-TC-Version": "2019-06-14",
+            "X-TC-Timestamp": str(int(time.time())),
+            "X-TC-Region": self.Region
         }
-        request = requests.post(req_url, headers=header, data=body)
+        # header['Authorization'] = \
+        self.makeSign(header, body)
+        
+        print('\n\n\n\nheaders:')
+        print(header)
+        
+        request = requests.post(req_url, headers=header, json=body)
         # 有些音频utf8解码失败，存在编码错误
         s = request.content.decode("utf8", "ignore")
         return s
 
+    def makeSign(self, headers, body):
+        def sign(key, msg):
+            return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
+        algorithm = "TC3-HMAC-SHA256"
+        service = "asr"
+        timestamp = int(headers['X-TC-Timestamp'])
+        date = datetime.utcfromtimestamp(timestamp).strftime("%Y-%m-%d")
+        http_request_method = "POST"
+        canonical_uri = "/"
+        canonical_querystring = ""
+        ct = headers['Content-Type']
+        payload = json.dumps(body)
+        host = headers['Host']
+        action = headers['X-TC-Action']
+        print("action: {}", action)
+        canonical_headers = "content-type:%s\nhost:%s\nx-tc-action:%s\n" % (ct, host, action.lower())
+        signed_headers = "content-type;host;x-tc-action"
+        hashed_request_payload = hashlib.sha256(payload.encode("utf-8")).hexdigest()
+        canonical_request = (http_request_method + "\n" +
+                             canonical_uri + "\n" +
+                             canonical_querystring + "\n" +
+                             canonical_headers + "\n" +
+                             signed_headers + "\n" +
+                             hashed_request_payload)
+        print(canonical_request)
+
+        # ************* 步骤 2：拼接待签名字符串 *************
+        credential_scope = date + "/" + service + "/" + "tc3_request"
+        hashed_canonical_request = hashlib.sha256(canonical_request.encode("utf-8")).hexdigest()
+        string_to_sign = (algorithm + "\n" +
+                          str(timestamp) + "\n" +
+                          credential_scope + "\n" +
+                          hashed_canonical_request)
+        print(string_to_sign)
+
+        # ************* 步骤 3：计算签名 *************
+        # 计算签名摘要函数
+        secret_date = sign(("TC3" + self.SECRET_KEY).encode("utf-8"), date)
+        secret_service = sign(secret_date, service)
+        secret_signing = sign(secret_service, "tc3_request")
+        signature = hmac.new(secret_signing, string_to_sign.encode("utf-8"), hashlib.sha256).hexdigest()
+        print(signature)
+
+        # ************* 步骤 4：拼接 Authorization *************
+        authorization = (algorithm + " " +
+                         "Credential=" + self.SECRET_ID + "/" + credential_scope + ", " +
+                         "SignedHeaders=" + signed_headers + ", " +
+                         "Signature=" + signature)
+        print(authorization)
+        
+        headers['Authorization'] = authorization
+        return authorization
+        
     # 拼接url和参数
     def formatSignString(self, config_dict):
-        signstr = "POSTaai.tencentcloudapi.com/?"
+        signstr = "POSTasr.tencentcloudapi.com/?"
         argArr = []
         for a, b in config_dict:
             argArr.append(a + "=" + str(b))
